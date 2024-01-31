4e7464fe7f973c2fd37d4a0e1c0736b08ac6a14d
SW ENG <sweng@massload.com>
2024-01-31 17:18:26 -0600

    using react hook and tanstack2

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import CreateModelVersionGroup from "./ModelManagement/Features/ModelTypeGroups/CreateModelVersionGroup";
 import ViewModelVersionGroup from "./ModelManagement/Features/ModelTypeGroups/ViewModelVersionGroup";
 
-
 function App() {
   return (
     <>
       {/* <h1>Akoms App- React Query - Complete Tutorial Cosden Solutions
  https://www.youtube.com/watch?v=8K1N3fE-cDs</h1>
- <h2>https://www.youtube.com/watch?v=3e-higRXoaM</h2>      */}
+ <h2>https://www.youtube.com/watch?v=3e-higRXoaM</h2>      
+ 
+ https://www.google.com/search?q=crud+with+react&rlz=1C1UEAD_enCA1075CA1075&oq=crud+with+rea&gs_lcrp=EgZjaHJvbWUqBwgAEAAYgAQyBwgAEAAYgAQyBggBEEUYOTIHCAIQABiABDIHCAMQABiABDIHCAQQABiABDIICAUQABgWGB4yCAgGEAAYFhgeMggIBxAAGBYYHjIICAgQABgWGB4yCAgJEAAYFhge0gEJNTk1NmowajE1qAIAsAIA&sourceid=chrome&ie=UTF-8#fpstate=ive&vld=cid:9ab4ed11,vid:DOkSU3yg4vQ,st:0
+ */}
       <ViewModelVersionGroup />
-      <CreateModelVersionGroup/>
+      <CreateModelVersionGroup />
     </>
   );
 }
