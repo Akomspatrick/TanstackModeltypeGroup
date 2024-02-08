194bb9d8e8d97f9cd0f372341a7dddb0ee49bce7
SW ENG <sweng@massload.com>
2024-02-08 11:40:47 -0600

    endpoints

--- src/ModelManagement/Services/TestingModeCRUDApi.ts
+++ src/ModelManagement/Services/TestingModeCRUDApi.ts
@@ -1,16 +1,16 @@
 import axios from 'axios';
-import { BaseURLs } from '../Constants';
-import { addData, deleteData, getAllData, getData, updateData } from './BaseCRUDApi';
-import { TestingModeGroupCreateRequestDTO, TestingModeGroupGetRequestDTO } from '../Types/ModelManagementInterfaces';
+import { BaseURLs,EndPoints } from '../../Constants/APIEndPoints';
+import { addData, deleteData, getAllData, getData, updateData } from '../../Services/BaseCRUDApi';
+import { TestingModeGroupCreateRequestDTO, TestingModeGroupResponseDTO } from '../Types/ModelManagementInterfaces';
 
 export default {
-  TestingModeCRUDApi(url = BaseURLs.ModelManagementURL + 'TestingModeGroups') {
+  TestingModeCRUDApi(url = BaseURLs.ModelManagementURL +EndPoints.MODEL_MANAGEMENT.TESTINGMODEGROUP) {
     const axiosInstance = axios.create({ baseURL: url });
 
     return {
       addData: (data: TestingModeGroupCreateRequestDTO) => {  return addData(axiosInstance, data); },
-      getAllData:  () => { return  getAllData<TestingModeGroupGetRequestDTO>(axiosInstance);},
-      getData: (query: string) => {return  getData<TestingModeGroupGetRequestDTO>(axiosInstance, query);},
+      getAllData:  () => { return  getAllData<TestingModeGroupResponseDTO>(axiosInstance);},
+      getData: (query: string) => {return  getData<TestingModeGroupResponseDTO>(axiosInstance, query);},
       updateData: (data: TestingModeGroupCreateRequestDTO) => {return   updateData(axiosInstance, data);},
       deleteData: (query: string) => {return  deleteData(axiosInstance, query);  },
     };
